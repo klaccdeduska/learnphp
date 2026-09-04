b1f1272ece09eabaa45fe550d66efdf926bb9550
Mihhail Burov <mihhail.burov@tptlive.ee>
2026-09-04 09:28:36 +0300

    Add types example

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "<h1>Hello World!<h1>";
+$test = 10;
+$test = 3.1;
+$test = "Meow";
+$test = "Õpilane";
+$test = "kakashki";
+$test = true;
+$test = false;
+$test = null;
+$test = [1, true, "poop"];
+$test = new stdClass();
+$test->name = "Miha";
+var_dump($test);
 ?>
