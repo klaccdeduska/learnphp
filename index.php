430fbb75aa59dc8f82cf0787a63f748800297689
Mihhail Burov <mihhail.burov@tptlive.ee>
2026-09-09 11:35:14 +0300

    Add array function examples

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $number = [1, 2, 3];
 var_dump($number[1]);
 $number[2] = 5;
 var_dump($number);
-$test = [1, 'dodik', true, [1, 2, 3]];
+$test = [1, 'budik', true, [1, 2, 3]];
 var_dump($test);
 var_dump($test[3][1]);
  
@@ -16,7 +16,23 @@ $test = [
     2,
     3,
     'color' => 'green',
+    'music' => 'pop',
+    5,
+    100 => 6,
+    7,
 ];
 var_dump($test);
 var_dump($test['name']);
+
+$combined = [1,2,3] + [4,5,6,7];
+var_dump($combined);
+
+$text = implode('-', $test);
+var_dump($text);
+
+array($test, 67, 'vue', 52);
+var_dump($text);
+$test[] = 'value';
+unset($test[102]);
+var_dump($test);
 ?>
