c2b6dc8909f304dbc4e0c481043fc127327590e3
Mihhail Burov <mihhail.burov@tptlive.ee>
2026-09-16 11:45:37 +0300

    Add visibillity example

--- index.php
+++ index.php
@@ -8,6 +8,22 @@ class box {
     public function volume() {
         return $this->width * $this->height * $this->length;
     }
+
+    public function setHeight(int $height) {
+        if ($height < 0) {
+            $this->height = 0;
+        } else {
+            $this->height = $height;
+        }
+    }
+    public function getHeight(int $height) {
+        return $this->height;
+    }
+
+        public function test2() {
+        $this->length = 10;
+        var_dump($this->length);
+    }
 }
 
 class MetalBox extends box {
@@ -15,8 +31,14 @@ class MetalBox extends box {
     public function mass() {
         return $this->weightPerUnit * $this->volume();
     }
+        public function test2() {
+        $this->height = 10;
+        var_dump($this->height);
+    }
 }
 
+$box = new box();
+var_dump($box);
 $metall = new MetalBox();
 var_dump($metall);
 ?>
