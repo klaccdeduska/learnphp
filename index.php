a601519adc17d426de4f16f108b4fbab295df650
Mihhail Burov <mihhail.burov@tptlive.ee>
2026-09-16 11:10:16 +0300

    add class and object example

--- index.php
+++ index.php
@@ -1,48 +1,28 @@
 <?php
-for($index = 0; $index < 10; $index++) {
-    var_dump($index);
-}
-for($index = 9; $index >= 0; $index--) {
-    var_dump($index);
-}
-for($index = 1; $index < 1000000; $index*=2) {
-    var_dump($index);
-}
 
-$time = time();
-$count = 0;
-while($time+1 > time()) {
-    $count++;
-}
-var_dump($count);
+class box {
+    public $width;
+    public $height;
+    public $legnth;
 
-while(false) {
-    var_dump('While');
-}
-
-do {
-    var_dump('Do');
-} while(false);
-
-$fruits = ['apple', 'banana', 'cherry'];
-
-for($index = 0; $index < count($fruits); $index++) {
-    $fruit = $fruits[$index];
-    var_dump($fruits[$index]);
-}
-foreach($fruits as $fruit) {
-    var_dump($fruit);
-}
-foreach($fruits as $key => $fruit) {
-    var_dump($key, $fruit);
-}
-
-function recursive($index) {
-    var_dump($index);
-    if($index < 10) {
-        recursive($index+1);
+    public function volume() {
+        return $this->width * $this->height * $this->legnth;
     }
 }
 
-recursive(0);
+$box1 = new box();
+$box1->width = 1;
+$box1->height = 2;
+$box1->legnth = 3;
+var_dump($box1);
+var_dump($box1->volume());
+
+$box2 = new box();
+$box2->width = 4;
+$box2->height = 5;
+$box2->legnth = 6;
+var_dump($box2);
+var_dump($box2->volume());
+var_dump($box1->volume());
+
 ?>
