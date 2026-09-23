368cb36541fdb310a6f94e975e45f7d49e05b4c9
Mihhail Burov <mihhail.burov@tptlive.ee>
2026-09-23 11:18:02 +0300

    Add php templanting example

--- index.php
+++ index.php
@@ -1,40 +1,25 @@
 <?php
-// library
-class Job {
-    public function task(Logger $logger) {
-       for ($i = 0; $i < 10; $i++) {
-        // we do some tasks
-         $logger->log("Task $i completed!");
-       }
-    }
-}
+$name = "Booblicki";
+?>
  
-class ConsoleLogger implements Logger {
-    public function log($message) {
-    echo $message . "\n";
-    }
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <h1> Hello, <?=$name?>! </h1>
+<ul>
  
- 
- 
-class NothingLogger implements Logger {
-    public function log($message) {
-   
-    }
-    }
- 
-interface Logger {
-    public function log($message);
-}
- 
-class FileLogger implements Logger {
-    public function log($message) {
-    $file = fopen("log.txt", "a");
-    fwrite($file, "$message\n");
-    fclose($file);
-    }
-}
-// user code
-$job = new Job();
-$logger = new FileLogger();
-$job->task($logger);
+    <?php for($i=0;$i<10;$i++): ?>
+        <?php if($i%2 === 0): ?>
+            <li style="color: green"> <?= $i?></li>
+        <?php else: ?>
+            <li style="color: red"> <?= $i?></li>
+        <?php endif ?>
+    <?php endfor ?>
+</ul>
+</body>
+</html>
